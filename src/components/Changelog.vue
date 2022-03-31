08e8d5430c6f0899cdb88b9ac58e9bced4092814
aclem43 <aclemeur@gmail.com>
2022-03-31 14:48:45 +1000

    Changed changelog colour bar

--- src/components/Changelog.vue
+++ src/components/Changelog.vue
@@ -8,7 +8,7 @@ const changelogs = getchangelogs()
 
 <template>
     <h1 class="text-xl px-5 py-2">Changelogs</h1>
-    <div class="bg-gradient-to-r to-secondary from-gray-900 mx-5 h-1 rounded-lg"></div>
+    <div class="bg-gradient-to-r from-secondary via-gray-900 to-secondary mx-5 h-1 rounded-lg"></div>
     <div class="mx-5">
         <div v-for="changelog in changelogs.CHANGELOGS" :key="changelog.version" class="px-2 py-3">
             <h1 class="text-lg">{{ changelog.version }}</h1>
